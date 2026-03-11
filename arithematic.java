aaff965168123780c5fd3820748eecf41650d4bb
Anos-Voldigold <demonkingoftyrany2000@gmail.com>
2026-03-11 09:28:47 +0530

    Update arithematic.java

--- arithematic.java
+++ arithematic.java
@@ -2,8 +2,8 @@ class P1
   {
     public static void main(String arg[])
     {
-      a = 20;
-      b = 10;
+      int a = 20;
+      int b = 10;
       System.out.println(a + b);
       System.out.println(a - b);
       System.out.println(a * b);
